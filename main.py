a31339071201a184403810de658314f09bbfe068
Arun jyothish <arunjyothishvikku@gmail.com>
2021-12-19 10:19:01 +0530

    minor errors

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import gpiozero
 import subprocess 
 import RPi.GPIO as gpio
 import time
-import board
 import Adafruit_DHT
 import psutil
 
@@ -18,6 +18,7 @@ for i in range(6):
 	gpio.setup(gpio_out_list[i],gpio.OUT)
 	gpio.setup(gpio_in_list[i],gpio.IN)
 
+gpio.setup(15,gpio.OUT)
 # motor pins
 # r1,r2 right side motor terminals
 # l1,l2 left side motor terminals
@@ -30,7 +31,7 @@ def dht():
 	for proc in psutil.process_iter():
 		if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
 			proc.kill()
-	sensor = adafuit_dht.DHT11(board.D23)
+	sensor = adafuit_dht.DHT11(16)
 
 	temp = sensor.temperature
 	humidity = sensor.humidity
@@ -46,80 +47,86 @@ def dht():
 
 class  motor:
 	def __init___(self,r1,r2,l1,l2):
-		gpio.setup(r1,gpio.OUT)
-		gpio.setup(r2,gpio.OUT)
-		gpio.setup(l1,gpio.OUT)
-		gpio.setup(l2,gpio.OUT)
+                self.r1 = r1
+                self.r2 = r2
+                self.l1 = l1 
+                self.l2 = l2
+		gpio.setup(self.r1,gpio.OUT)
+		gpio.setup(self.r2,gpio.OUT)
+		gpio.setup(self.l1,gpio.OUT)
+		gpio.setup(self.l2,gpio.OUT)
 		
 		def forward(self,r1,r2):
-			gpio.output(r1,gpio.HIGH)
-			gpio.output(r2,gpio.LOW)
-			gpio.output(l1,gpio.HIGH)
-			gpio.output(l2,gpio.LOW)
+			gpio.output(self.r1,gpio.HIGH)
+			gpio.output(self.r2,gpio.LOW)
+			gpio.output(self.l1,gpio.HIGH)
+			gpio.output(self.l2,gpio.LOW)
 
 		def backward(self,r1,r2):
-			gpio.output(r1,gpio.LOW)
-			gpio.output(r2,gpio.HIGH)
-			gpio.output(l1,gpio.LOW)
-			gpio.output(l2,gpio.HIGH)
+			gpio.output(self.r1,gpio.LOW)
+			gpio.output(self.r2,gpio.HIGH)
+			gpio.output(self.l1,gpio.LOW)
+			gpio.output(self.l2,gpio.HIGH)
 			
 		def right_turn(self,r1,r2):
-			gpio.output(r1,gpio.HIGH)
-			gpio.output(r2,gpio.LOW)
-			gpio.output(l1,gpio.LOW)
-			gpio.output(l2,gpio.HIGH)
+			gpio.output(self.r1,gpio.HIGH)
+			gpio.output(self.r2,gpio.LOW)
+			gpio.output(self.l1,gpio.LOW)
+			gpio.output(self.l2,gpio.HIGH)
 			
 		def left_turn(self,r1,r2):
-			gpio.output(r1,gpio.LOW)
-			gpio.output(r2,gpio.HIGH)
-			gpio.output(l1,gpio.HIGH)
-			gpio.output(l2,gpio.LOW)
-
-class Dist:
-	def __init__(self,trig,echo):
-		self.trig = trig
-		self.echo = echo
-
-
-	def distance(self):
-		gpio.output(self.trig, True)
-
-# set Trigger after 0.01ms to LOW
-		time.sleep(0.00001)
-		gpio.output(self.trig,True )
-
-		StartTime = time.time()
-		StopTime = time.time()
-
-# save StartTime
-		while gpio.input(self.echo) == 0:
-			StartTime = time.time()
-
-# save time of arrival
-		while gpio.input(self.echo) == 1:
-			StopTime = time.time()
-
-# time difference between start and arrival
-		TimeElapsed = StopTime - StartTime
-# multiply with the sonic speed (34300 cm/s)
-# and divide by 2, because there and back
-		distance = (TimeElapsed * 34300) / 2
-
-		return distance
+			gpio.output(self.r1,gpio.LOW)
+			gpio.output(self.r2,gpio.HIGH)
+			gpio.output(self.l1,gpio.HIGH)
+			gpio.output(self.l2,gpio.LOW)
+# 
+# class Dist:
+# 	def __init__(self,trig,echo):
+# 		self.trig = trig
+# 		self.echo = echo
+# 
+# 
+# 	def distance(self):
+# 		gpio.output(self.trig, True)
+# 
+# # set Trigger after 0.01ms to LOW
+# 		time.sleep(0.00001)
+# 		gpio.output(self.trig,True )
+# 
+# 		StartTime = time.time()
+# 		StopTime = time.time()
+# 
+# # save StartTime
+# 		while gpio.input(self.echo) == 0:
+# 			StartTime = time.time()
+# 
+# # save time of arrival
+# 		while gpio.input(self.echo) == 1:
+# 			StopTime = time.time()
+# 
+# # time difference between start and arrival
+# 		TimeElapsed = StopTime - StartTime
+# # multiply with the sonic speed (34300 cm/s)
+# # and divide by 2, because there and back
+# 		distance = (TimeElapsed * 34300) / 2
+# 
+# 		return distance
 	
 
 
-fl_ul = Dist(2,3)
-br_ul = Dist(17,27)
-ff_ul = Dist(10,9)
-bb_ul = Dist(0,5)
-fr_ul = Dist(6,13)
-bl_ul = Dist(19,26)
-	
+# fr_ul = gpiozero.DistanceSensor(2,3)
+fl_ul = gpiozero.DistanceSensor(17,27)
+ff_ul = gpiozero.DistanceSensor(10,9)
+br_ul = gpiozero.DistanceSensor(0,5)
+bb_ul = gpiozero.DistanceSensor(6,13)
+bl_ul = gpiozero.DistanceSensor(19,26)
+
+
+
+drive  = motor(20,21,7,1)
+
 def loop():
 
-	right_M = motor(20,21)
-	left_M = motor(7,1)
 		
 	min_dist = 50 
 
@@ -141,10 +148,13 @@ def loop():
 	print("Temperature: {}*C   Humidity: {}% ".format(temp, humidity))
 print("fn")
 while True:
-	
-	
+        
+	print("in loop")	
 	time.sleep(2)
-	print(fr_ul.distance())
+        # gpio.output(15,gpio.HIGH)
+        drive.forward()
 	time.sleep(2)
-
 	print("fl-end")
+
+
+
