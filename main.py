9f39b45ee95aab69a30d19070501629e72e02da2
Arun jyothish <arunjyothishvikku@gmail.com>
2021-12-19 11:12:04 +0530

    just working

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ gpio.setmode(gpio.BCM)
 
 # gpio setmode 
 
-gpio_out_list = [2,17,10,0,6,19]
+gpio_out_list = [2,17,10,0,6,19,20,21,7,15,1]
 gpio_in_list = [3,27,9,5,13,26]
 
-for i in range(6):
+for i in range(11):
 	gpio.setup(gpio_out_list[i],gpio.OUT)
-	gpio.setup(gpio_in_list[i],gpio.IN)
+	if i<6:
+		gpio.setup(gpio_in_list[i],gpio.IN)
 
-gpio.setup(15,gpio.OUT)
+#gpio.setup(15,gpio.OUT)
 # motor pins
 # r1,r2 right side motor terminals
 # l1,l2 left side motor terminals
@@ -45,85 +46,81 @@ def dht():
 #		raise error
 	return humidity,temp
 
-class  motor:
-	def __init___(self,r1,r2,l1,l2):
-                self.r1 = r1
-                self.r2 = r2
-                self.l1 = l1 
-                self.l2 = l2
-		gpio.setup(self.r1,gpio.OUT)
-		gpio.setup(self.r2,gpio.OUT)
-		gpio.setup(self.l1,gpio.OUT)
-		gpio.setup(self.l2,gpio.OUT)
+# class  motor():
+# 	def __init___(self,r1,r2,l1,l2):
+#                 self.r1 = r1
+#                 self.r2 = r2
+#                 self.l1 = l1 
+#                 self.l2 = l2
+# 		gpio.setup(self.r1,gpio.OUT)
+# 		gpio.setup(self.r2,gpio.OUT)
+# 		gpio.setup(self.l1,gpio.OUT)
+# 		gpio.setup(self.l2,gpio.OUT)
 		
-		def forward(self,r1,r2):
-			gpio.output(self.r1,gpio.HIGH)
-			gpio.output(self.r2,gpio.LOW)
-			gpio.output(self.l1,gpio.HIGH)
-			gpio.output(self.l2,gpio.LOW)
+def forward(r1,r2,l1,l2):
+	gpio.output(r1,gpio.HIGH)
+	gpio.output(r2,gpio.LOW)
+	gpio.output(l1,gpio.HIGH)
+	gpio.output(l2,gpio.LOW)
 
-		def backward(self,r1,r2):
-			gpio.output(self.r1,gpio.LOW)
-			gpio.output(self.r2,gpio.HIGH)
-			gpio.output(self.l1,gpio.LOW)
-			gpio.output(self.l2,gpio.HIGH)
-			
-		def right_turn(self,r1,r2):
-			gpio.output(self.r1,gpio.HIGH)
-			gpio.output(self.r2,gpio.LOW)
-			gpio.output(self.l1,gpio.LOW)
-			gpio.output(self.l2,gpio.HIGH)
-			
-		def left_turn(self,r1,r2):
-			gpio.output(self.r1,gpio.LOW)
-			gpio.output(self.r2,gpio.HIGH)
-			gpio.output(self.l1,gpio.HIGH)
-			gpio.output(self.l2,gpio.LOW)
-# 
-# class Dist:
-# 	def __init__(self,trig,echo):
-# 		self.trig = trig
-# 		self.echo = echo
-# 
-# 
-# 	def distance(self):
-# 		gpio.output(self.trig, True)
-# 
-# # set Trigger after 0.01ms to LOW
-# 		time.sleep(0.00001)
-# 		gpio.output(self.trig,True )
-# 
-# 		StartTime = time.time()
-# 		StopTime = time.time()
-# 
-# # save StartTime
-# 		while gpio.input(self.echo) == 0:
-# 			StartTime = time.time()
-# 
-# # save time of arrival
-# 		while gpio.input(self.echo) == 1:
-# 			StopTime = time.time()
-# 
-# # time difference between start and arrival
-# 		TimeElapsed = StopTime - StartTime
-# # multiply with the sonic speed (34300 cm/s)
-# # and divide by 2, because there and back
-# 		distance = (TimeElapsed * 34300) / 2
-# 
-# 		return distance
+def backward(r1,r2,l1,l2):
+	gpio.output(r1,gpio.LOW)
+	gpio.output(r2,gpio.HIGH)
+	gpio.output(l1,gpio.LOW)
+	gpio.output(l2,gpio.HIGH)
 	
+def right_turn(r1,r2,l1,l2):
+	gpio.output(r1,gpio.HIGH)
+	gpio.output(r2,gpio.LOW)
+	gpio.output(l1,gpio.LOW)
+	gpio.output(l2,gpio.HIGH)
+	
+def left_turn(r1,r2,l1,l2):
+	gpio.output(r1,gpio.LOW)
+	gpio.output(r2,gpio.HIGH)
+	gpio.output(l1,gpio.HIGH)
+	gpio.output(l2,gpio.LOW)
+# 
+class Dist:
+	def __init__(self,trig,echo):
+		self.trig = trig
+		self.echo = echo
 
 
-# fr_ul = gpiozero.DistanceSensor(2,3)
-fl_ul = gpiozero.DistanceSensor(17,27)
-ff_ul = gpiozero.DistanceSensor(10,9)
-br_ul = gpiozero.DistanceSensor(0,5)
-bb_ul = gpiozero.DistanceSensor(6,13)
-bl_ul = gpiozero.DistanceSensor(19,26)
+	def distance(self):
+		gpio.output(self.trig, True)
 
+# set Trigger after 0.01ms to LOW
+		time.sleep(0.00001)
+		gpio.output(self.trig,True )
 
+		StartTime = time.time()
+		StopTime = time.time()
 
-drive  = motor(20,21,7,1)
+# save StartTime
+		while gpio.input(self.echo) == 0:
+			StartTime = time.time()
+
+# save time of arrival
+		while gpio.input(self.echo) == 1:
+			StopTime = time.time()
+
+# time difference between start and arrival
+		TimeElapsed = StopTime - StartTime
+# multiply with the sonic speed (34300 cm/s)
+# and divide by 2, because there and back
+		distance = (TimeElapsed * 34300) / 2
+
+		return distance
+	
+fl_ul = Dist(2,3)
+br_ul = Dist(17,27)
+ff_ul = Dist(10,9)
+bb_ul = Dist(0,5)
+fr_ul = Dist(6,13)
+bl_ul = Dist(19,26)
+
+# drive  = motor(20,21,7,1)
 
 def loop():
 
@@ -151,10 +148,22 @@ while True:
         
 	print("in loop")	
 	time.sleep(2)
-        # gpio.output(15,gpio.HIGH)
-        drive.forward()
+        gpio.output(15,gpio.LOW)
+#print(br_ul.distance())
+#drive.forward()
 	time.sleep(2)
 	print("fl-end")
 
 
+	r1,r2,l1,l2=20,21,7,1
 
+	forward(r1,r2,l1,l2)
+	time.sleep(2)
+	right_turn(r1,r2,l1,l2)
+	time.sleep(2)
+	left_turn(r1,r2,l1,l2)
+	time.sleep(2)
+	right_turn(r1,r2,l1,l2)
+	time.sleep(2)
+	backward(r1,r2,l1,l2)
+	time.sleep(2)
